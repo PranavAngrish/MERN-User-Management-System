957a2183da716a83781fc3c44f44ea024776a591
JackWorld <jackwong0326@gmail.com>
2022-09-28 15:10:38 +0800

    Initial views, routes and layout.

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          {user && (
+            <>
+              <span>{user.email}</span>
+              <button onClick={() => logout()}>Log out</button>
+            </>
+          )}
+          {!user && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
