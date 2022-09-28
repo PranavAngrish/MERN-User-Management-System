b7fab7887e7ab823a8000300509be7bde19f433a
JackWorld <jackwong0326@gmail.com>
2022-09-29 02:03:47 +0800

    Protect react routes and authorized request.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,21 @@
+import { useEffect }from 'react'
+import { useAuthContext } from "../hooks/useAuthContext"
+
 const Home = () => {
+  const {user} = useAuthContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('', {
+        headers: {'Authorization': `Bearer ${user.token}`},
+      })
+      const json = await response.json()
+
+      if (response.ok){}
+    }
+    if (user) {}
+  }, [user])
+
   return (
     <div className="home"></div>
   )
