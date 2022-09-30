dbcf6aed289418bb935df634c0710623fde399c7
JackWorld <jackwong0326@gmail.com>
2022-10-01 01:08:18 +0800

    Added error handling logs.

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const { logger } = require('./middleware/logger')
-const authRoute = require('./routes/auth')
-const sleepRoutes = require('./routes/sleep')
-const connectDB = require('./config/dbConn')
+const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const connectDB = require('./config/dbConn')
 require('dotenv').config()
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 const app = express()
 
-// app.use(logger)
-
 connectDB()
+// app.use(logger)
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
-app.use('/api/user', authRoute)
-app.use('/api/sleeps', sleepRoutes)
+app.use('/api/user', require('./routes/auth'))
+app.use('/api/sleeps', require('./routes/sleep'))
+// app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
   console.log('Databse Connected Successfully!')
   app.listen(port, () => console.log(`Server running on port ${port}`))
 })
+
