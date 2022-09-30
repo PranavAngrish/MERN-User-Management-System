c3b3c69425bdfbb91e739c13d684d6a703572d70
JackWorld <jackwong0326@gmail.com>
2022-10-01 01:03:00 +0800

    Clear sleep data when logout.

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from './useAuthContext'
+import { useSleepsContext } from './useSleepsContext'
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext()
+  const { dispatch: dispatchSleeps } = useSleepsContext()
 
   const logout = () => {
     localStorage.removeItem('user')
     dispatch({ type: 'LOGOUT' })
+    dispatchSleeps({ type: 'SET_SLEEPS', payload: null })
   }
 
   return { logout }
---
+++ frontend/src/hooks/useSleepsContext.js
@@ -0,0 +1,9 @@
+import { useContext } from "react"
+import { SleepsContext } from "../context/sleep"
+
+export const useSleepsContext = () => {
+  const context = useContext(SleepsContext)
+  if(!context) throw Error('useSleepsContext must be used inside an SleepsContextProvider')
+
+  return context
+}
