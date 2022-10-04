22375a2c8b7fa47a976f6ca22243d77892e8c3db
JackWorld <jackwong0326@gmail.com>
2022-10-04 21:40:35 +0800

    Update navbar style with bootstrap.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,34 +1,38 @@
-import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
+import { Container, Nav, Navbar, Button } from "react-bootstrap"
 
-const Navbar = () => {
+const Navbars = () => {
   const { logout } = useLogout()
   const { user } = useAuthContext()
 
   return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Go Sleep Buddy!!!</h1>
-        </Link>
-        <nav>
-          {user && (
-            <>
-              <span>{user.name}</span>
-              <button onClick={() => logout()}>Log Out</button>
-            </>
-          )}
-          {!user && (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><h3>Go Sleep Buddy!!!</h3></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              {user && (
+                <>
+                  <Navbar.Text>{user.name}</Navbar.Text>
+                  <Button variant="outline-warning" className="mx-3" onClick={() => logout()}>Log Out</Button>
+                </>
+              )}
+              {!user && (
+                <>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                  <Nav.Link href="/signup">Signup</Nav.Link>
+                </>
+              )}
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default Navbars
