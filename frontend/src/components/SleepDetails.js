dda302ef6bbbe2371579ed22531b9aed90a28612
JackWorld <jackwong0326@gmail.com>
2022-10-10 22:02:04 +0800

    Remove useless fetch request.

--- frontend/src/components/SleepDetails.js
+++ frontend/src/components/SleepDetails.js
@@ -30,25 +30,6 @@ const SleepDetails = ({ sleep }) => {
     } catch (error) {
       setError(error.response?.data.error)
     }
-
-    const response = await fetch('/api/sleeps/' + sleep._id, {
-      method: 'DELETE',
-      headers: {
-        'Authorization': `Bearer ${user.accessToken}`
-      }
-    })
-
-    const json = await response.json()
-    console.log(json)
-
-    if (!response.ok) {
-      setError(json.error)
-    }
-
-    if (response.ok) {
-      setError(null)
-      dispatch({type: 'DELETE_SLEEP', payload: json})
-    }
   }
 
   const handleUpdate = async () => {
@@ -82,55 +63,55 @@ const SleepDetails = ({ sleep }) => {
     }
   }
 
-    return (
-      <>
-        <div className="card my-3">
-          <div className="card-body d-flex justify-content-between align-items-start">
-            <div>
-              <div className="fs-4 mb-1 text-primary">{sleep.title}</div>
-              <p>
-                <strong className="fs-6 text-muted">Load (kg): </strong>{sleep.load}<br/>
-                <strong className="fs-6 text-muted">Number of reps: </strong>{sleep.reps}<br/>
-                {formatDistanceToNow(new Date(sleep.createdAt), { addSuffix: true })}
-              </p>
-            </div>
-            <div>
-              <button className="btn btn-sm btn-outline-primary rounded-circle mx-2 p-2" onClick={() => setShow(!show)}><BsPencilSquare className="fs-5"/></button>
-              <button className="btn btn-outline-danger rounded-circle p-2" onClick={handleDelete}><BsFillTrashFill className="fs-5"/></button>
-            </div>
+  return (
+    <>
+      <div className="card my-3">
+        <div className="card-body d-flex justify-content-between align-items-start">
+          <div>
+            <div className="fs-4 mb-1 text-primary">{sleep.title}</div>
+            <p>
+              <strong className="fs-6 text-muted">Load (kg): </strong>{sleep.load}<br/>
+              <strong className="fs-6 text-muted">Number of reps: </strong>{sleep.reps}<br/>
+              {formatDistanceToNow(new Date(sleep.createdAt), { addSuffix: true })}
+            </p>
           </div>
-          <div className="mx-3">
-            {error && (<div className="alert alert-danger" role="alert">{error}</div>)}
+          <div>
+            <button className="btn btn-sm btn-outline-primary rounded-circle mx-2 p-2" onClick={() => setShow(!show)}><BsPencilSquare className="fs-5"/></button>
+            <button className="btn btn-outline-danger rounded-circle p-2" onClick={handleDelete}><BsFillTrashFill className="fs-5"/></button>
           </div>
         </div>
+        <div className="mx-3">
+          {error && (<div className="alert alert-danger" role="alert">{error}</div>)}
+        </div>
+      </div>
 
-        <Modal show={show} onHide={() => {setShow(!show);setError(null)}} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>Edit Sleep Record</Modal.Title>
-          </Modal.Header> 
-          <Modal.Body>
-            <Form.Group className="mb-3">
-              <Form.Label>Excersize Title:</Form.Label>
-              <Form.Control type="text" placeholder={sleep.title} autoFocus ref={titleRef}/>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Load (in kg):</Form.Label>
-              <Form.Control type="number" placeholder={sleep.load} autoFocus ref={loadRef}/>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Number of Reps:</Form.Label>
-              <Form.Control type="number" placeholder={sleep.reps} autoFocus ref={repsRef}/>
-            </Form.Group>
-            {error && (<Alert variant={'danger'}>{error}</Alert>)}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleUpdate}> 
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    )
-  }
+      <Modal show={show} onHide={() => {setShow(!show);setError(null)}} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Sleep Record</Modal.Title>
+        </Modal.Header> 
+        <Modal.Body>
+          <Form.Group className="mb-3">
+            <Form.Label>Excersize Title:</Form.Label>
+            <Form.Control type="text" placeholder={sleep.title} autoFocus ref={titleRef}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Load (in kg):</Form.Label>
+            <Form.Control type="number" placeholder={sleep.load} autoFocus ref={loadRef}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Number of Reps:</Form.Label>
+            <Form.Control type="number" placeholder={sleep.reps} autoFocus ref={repsRef}/>
+          </Form.Group>
+          {error && (<Alert variant={'danger'}>{error}</Alert>)}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleUpdate}> 
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
   
-  export default SleepDetails
+export default SleepDetails
