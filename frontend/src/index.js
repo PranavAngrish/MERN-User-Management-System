c028c8d9272b99a01ac93daf50cf2ea30505cc0b
JackWorld <jackwong0326@gmail.com>
2022-10-12 05:15:44 +0800

    Disable react dev tools in production.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import './index.css'
 import App from './App'
 import { AuthContextProvider } from './context/auth'
 import { SleepsContextProvider } from './context/sleep'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
