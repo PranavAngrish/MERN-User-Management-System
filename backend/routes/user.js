bdca8734acdc0fa413afa226cf2fcdb4dfef43d8
JackWorld <jackwong0326@gmail.com>
2022-10-19 17:09:21 +0800

    Add user delete feature.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,6 +8,8 @@ router.route('/')
     .get(usersController.getAll)
     .post(usersController.create)
     .patch(usersController.update)
+
+router.route('/:id')
     .delete(usersController.delete)
 
 module.exports = router
--- frontend/src/components/users/Delete.js
+++ frontend/src/components/users/Delete.js
@@ -1,20 +1,53 @@
-import React from 'react'
+import { useState } from 'react'
+import { Modal, Button } from 'react-bootstrap'
 import { BsFillTrashFill } from 'react-icons/bs'
-import { Modal } from 'react-bootstrap'
+import { GoAlert } from 'react-icons/go'
+import { useUserContext } from '../../hooks/useUserContext'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 
-const Delete = () => {
+const Delete = ({user}) => {
+  const axiosPrivate = useAxiosPrivate()
+  const { dispatch } =  useUserContext()
+  const { auth } = useAuthContext()
+  const [error, setError] = useState(null)
+  const [show, setShow] = useState(false)
+
+  const handleDelete = async () => {
+    if(!auth) {
+      setError('You must be logged in') 
+      setShow(!show)
+      return
+    }
+
+    try {
+      const response = await axiosPrivate.delete('/api/users/' + user._id)
+      dispatch({type: 'DELETE_USER', payload: response.data})
+      setError(null)
+      setShow(!show)
+    } catch (error) {
+      // console.log(error)
+    }
+  }
+
   return (
     <>
-      <button className="btn btn-outline-danger p-1"><BsFillTrashFill className="fs-4"/></button>
+      <button className="btn btn-outline-danger p-1" onClick={() => setShow(!show)}><BsFillTrashFill className="fs-4"/></button>
       
-      {/* <Modal show={show} onHide={() => {setShow(!show);setError(null)}} centered>
-          <Modal.Header closeButton>
-              <Modal.Title>Error</Modal.Title>
-          </Modal.Header> 
-          <Modal.Body closeButton>
-              <div className="alert alert-danger" role="alert">{error}dfgdfdf dfhdfhdfgh dffhgdfh </div>
-          </Modal.Body>
-      </Modal> */}
+      <Modal show={show} onHide={() => {setShow(!show);setError(null)}} centered>
+        <Modal.Header closeButton>
+          {!error && (<Modal.Title className="d-inline-flex align-items-center"><GoAlert/>&nbsp;Warning</Modal.Title>)}
+          {error && (<Modal.Title>Error</Modal.Title>)}
+        </Modal.Header> 
+        <Modal.Body closeButton>
+          Are you sure, delete user <strong>{user.name}</strong> ?
+          {error && (<div className="alert alert-danger" role="alert">{error}</div>)}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleDelete}>Delete</Button>
+          <Button variant="secondary" onClick={() => setShow(!show)}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
