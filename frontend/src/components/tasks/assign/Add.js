cc6543c11dac93d0c68054c1f00346e3dcb31a35
JackWorld <jackwong0326@gmail.com>
2022-11-03 03:02:23 +0800

    Features assign task: Error handle.

--- frontend/src/components/tasks/assign/Add.js
+++ frontend/src/components/tasks/assign/Add.js
@@ -5,10 +5,11 @@ import { useTasksContext } from '../../../hooks/useTasksContext'
 import useAxiosPrivate from '../../../hooks/useAxiosPrivate'
 
 const Add = ({ task_id }) => {
-  const [show, setShow] = useState(false)
+  const axiosPrivate = useAxiosPrivate()
   const { setAssignedUser } =  useTasksContext()
   const [notAssignedUser, setNotAssignedUser] = useState([])
-  const axiosPrivate = useAxiosPrivate()
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(null)
   const nameRef = useRef()
 
   const handleClick = async () => {
@@ -17,14 +18,23 @@ const Add = ({ task_id }) => {
     try {
       const response = await axiosPrivate.get(`/api/users/unassigned/${task_id}`)
       setNotAssignedUser(response.data)
+      setError(null)
+      if(response.data.length === 0){
+        setError("No idle users found")
+      }
     } catch (err) {
       // console.log(err)
+      setError(err.response.data.error)
     }
-
   }
 
   const handleAssign = async () => {
     const seletedUser = Array.from(nameRef.current.selectedOptions, option => option.value)
+    if(seletedUser.length === 0){
+      setShow(true)
+      setError("No user seleted")
+      return
+    }
 
     try {
       const response = await axiosPrivate.post('/api/tasks/assign', {
@@ -32,9 +42,11 @@ const Add = ({ task_id }) => {
         user_id: seletedUser
       }) 
       setAssignedUser(response.data)
+      setError(null)
       setShow(false)
     } catch (error) {
       // console.log(error)
+      setError(error.response.data.error)
     }
   }
 
@@ -43,20 +55,21 @@ const Add = ({ task_id }) => {
       <button className="btn btn-outline-primary mb-2" onClick={handleClick}><BsPlusLg /></button>
 
       <Modal show={show} onHide={() => {setShow(!show)}} centered>
-      <Modal.Header closeButton>
-        <Modal.Title className="d-inline-flex align-items-center">Assign User</Modal.Title>
-      </Modal.Header> 
-      <Modal.Body>
-      <select className="form-select" multiple size="5" aria-label="multiple select user" ref={nameRef}>
-        {notAssignedUser.map((n, index) => (
-          <option value={n._id} key={index}>{n.name}</option>
-        ))}
-      </select>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="primary" onClick={handleAssign}>Assign</Button>
-      </Modal.Footer>
-    </Modal>
+        <Modal.Header closeButton>
+          <Modal.Title className="d-inline-flex align-items-center">Assign User</Modal.Title>
+        </Modal.Header> 
+        <Modal.Body>
+          {!(notAssignedUser.length === 0) && (<select className="form-select" multiple size="5" aria-label="multiple select user" ref={nameRef}>
+            {notAssignedUser.map((n, index) => (
+              <option value={n._id} key={index}>{n.name}</option>
+            ))}
+          </select>)}
+          {error && (<div className="alert alert-danger mt-3" role="alert">{error}</div>)}
+        </Modal.Body>
+        {!(notAssignedUser.length === 0) && (<Modal.Footer>
+          <Button variant="primary" onClick={handleAssign}>Assign</Button>
+        </Modal.Footer>)}
+      </Modal>  
   </>
   )
 }
