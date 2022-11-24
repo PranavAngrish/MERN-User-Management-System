39d319f1bf62333c0c3d3cca7b1c389e6cbaa816
JackWorld <jackwong0326@gmail.com>
2022-11-24 23:54:44 +0800

    Update note management create note features code logic.

--- frontend/src/components/notes/Add.js
+++ frontend/src/components/notes/Add.js
@@ -1,16 +1,74 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { BiArrowBack } from 'react-icons/bi'
-import { BsPlusLg } from 'react-icons/bs'
+import { useRef, useState } from 'react'
+import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { useNotesContext } from '../../hooks/useNotesContext'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useUserContext } from '../../hooks/useUserContext'
+import { Link } from 'react-router-dom'
+import { ROLES } from '../../config/roles'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 
 const Add = () => {
-  const navigate = useNavigate()
+  const axiosPrivate = useAxiosPrivate()
+  const { auth } = useAuthContext()
+  const { targetUser } =  useUserContext()
+  const { dispatch } = useNotesContext()
+  const [ error, setError ] = useState(null)
+  const titleRef = useRef('')
+  const textRef = useRef('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!auth) {
+      setError('You must be logged in')
+      return
+    }
+    
+    try {
+      const rughtToAdd = auth.roles == ROLES.Admin || auth.roles == ROLES.Root
+      const note = {title: titleRef.current.value, text: textRef.current.value}
+
+      if(targetUser?.userId && (auth.email !== targetUser?.userEmail) && (rughtToAdd)){
+        note.id = targetUser.userId
+      }
+
+      const response = await axiosPrivate.post('/api/note', note)
+      setError(null)
+      dispatch({type: 'CREATE_NOTE', payload: response.data})
+    } catch (error) {
+      // console.log(error)
+      setError(error.response?.data.error)
+    }
+  }
   
   return (
-    <div className="d-flex justify-content-between">
-      <button className="btn btn-outline-primary mb-2" onClick={() => navigate('/', {replace: true})}><BiArrowBack /></button>
-      <button className="btn btn-outline-primary mb-2"><BsPlusLg /></button>
-    </div>
+    <>
+      <h1 className="my-3">New Note</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <Stack gap={4}>
+          <Row>
+            <Col>
+              <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control ref={titleRef} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Form.Group controlId="markdown">
+            <Form.Label>Body</Form.Label>
+            <Form.Control as="textarea" ref={textRef} rows={15}/>
+          </Form.Group>
+          {error && (<Alert variant={'danger'}>{error}</Alert>)}
+          <Stack direction="horizontal" gap={2} className="justify-content-end">
+            <Button type="submit" variant="primary">Save</Button>
+            <Link to="..">
+              <Button type="button" variant="outline-secondary">Cancel</Button>
+            </Link>
+          </Stack>
+        </Stack>
+      </Form>
+    </>
   )
 }
 
--- frontend/src/pages/Note.js
+++ frontend/src/pages/Note.js
@@ -1,11 +1,15 @@
 import { useEffect } from 'react'
 import { GoSearch } from "react-icons/go"
+import { BiArrowBack } from 'react-icons/bi'
+import { BsPlusLg } from 'react-icons/bs'
+import { Link, useNavigate } from "react-router-dom"
 import { usePathContext } from '../context/path'
 import Details from '../components/notes/Index'
 import Add from '../components/notes/Add'
 
 const Note = () => {
   const { setTitle } = usePathContext()
+  const navigate = useNavigate()
 
   useEffect(() => {
     setTitle("Note Management")
@@ -13,7 +17,10 @@ const Note = () => {
   
   return (
     <>
-      <Add />
+      <div className="d-flex justify-content-between">
+        <button className="btn btn-outline-primary mb-2" onClick={() => navigate('/note', {replace: true})}><BiArrowBack /></button>
+        <button className="btn btn-outline-primary mb-2" onClick={() => navigate('/note/add')}><BsPlusLg /></button>
+      </div>
 
       <div className="input-group mt-2 mb-3">
         <input type="search" className="form-control" placeholder="Search..."/>
