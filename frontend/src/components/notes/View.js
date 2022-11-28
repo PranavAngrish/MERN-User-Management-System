85e0c1705abe581ac647c4629c437c48c795ca0c
JackWorld <jackwong0326@gmail.com>
2022-11-28 23:44:34 +0800

    Show note markdown with react-markdown package.

--- frontend/src/components/notes/View.js
+++ frontend/src/components/notes/View.js
@@ -1,11 +1,61 @@
+import { useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import { Button, Col, Row, Stack } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { usePathContext } from '../context/path'
+import { useUserContext } from '../hooks/useUserContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useNoteContext } from '../../context/note'
+import useAxiosPrivate from "../hooks/useAxiosPrivate"
+import ReactMarkdown from "react-markdown"
 
 const View = () => {
+  const navigate = useNavigate()
+  const { setTitle } = usePathContext()
+  const { auth } = useAuthContext()
+  const { targetUser } = useUserContext()
+  const { notes, dispatch } = useNoteContext()
+  const axiosPrivate = useAxiosPrivate()
+
+  useEffect(() => {
+    let isMounted = true
+    const abortController = new AbortController()
+    setTitle("Note Management")
+
+    const getNoteList = async () => {
+      try {
+        let response
+        if(targetUser?.userId && (auth.email !== targetUser.userEmail) && (auth.roles == ROLES.Admin)){
+          // Admin view
+          response = await axiosPrivate.post('/api/notes/admin', {
+            id: targetUser.userId,
+            signal: abortController.signal
+          })
+        }else{
+          response = await axiosPrivate.get('/api/notes', {
+            signal: abortController.signal
+          })
+        }
+        isMounted && dispatch({type: 'SET_NOTE', payload: response.data})
+      } catch (err) {
+        dispatch({type: 'SET_NOTE', payload: []})
+        // console.log(err)
+      }
+    }
+
+    if(auth){
+      getNoteList()
+    }
+
+    return () => {
+      isMounted = false
+      abortController.abort()
+    }
+  },[])
+
   return (
     <>
       <Row className="align-items-center mb-4">
-        <Col><h1>title</h1></Col>
+        <Col><h1>{note.title}</h1></Col>
         <Col xs="auto">
           <Stack gap={2} direction="horizontal">
             <Link to="/note/edit">
@@ -18,6 +68,7 @@ const View = () => {
           </Stack>
         </Col>
       </Row>
+      <ReactMarkdown>{note.body}</ReactMarkdown>
     </>
   )
 }
