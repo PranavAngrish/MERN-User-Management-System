954a9a28710287622d925c55d5f73742e0de37f1
JackWorld <jackwong0326@gmail.com>
2022-11-30 23:56:40 +0800

    Features: Remove note.

--- frontend/src/components/notes/View.js
+++ frontend/src/components/notes/View.js
@@ -1,13 +1,16 @@
 import { useEffect } from 'react'
 import { ROLES } from '../../config/roles'
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { BiArrowBack } from 'react-icons/bi'
+import { BsPencilSquare, BsFillTrashFill } from 'react-icons/bs'
+import { useNavigate, useParams } from "react-router-dom"
 import { Button, Col, Row, Stack } from "react-bootstrap"
 import { usePathContext } from '../../context/path'
+import { useNoteContext } from '../../context/note'
 import { useUserContext } from '../../hooks/useUserContext'
 import { useAuthContext } from '../../hooks/useAuthContext'
-import { useNoteContext } from '../../context/note'
 import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 import ReactMarkdown from "react-markdown"
+import axios from '../api/axios' 
 
 const View = () => {
   const navigate = useNavigate()
@@ -38,7 +41,6 @@ const View = () => {
             signal: abortController.signal
           })
         }
-        console.log(response.data)
         isMounted && dispatch({type: 'SET_NOTE', payload: response.data})
       } catch (err) {
         dispatch({type: 'SET_NOTE', payload: []})
@@ -56,6 +58,15 @@ const View = () => {
     }
   },[])
 
+  const deleteNote = async () => {
+    try {
+      const response = await axios.delete(`/api/notes/${id}`)
+      dispatch({ type: 'DELETE_NOTE', payload: response.data })
+    } catch (error) {
+      // console.log(error)
+    }
+  }
+
   return (
     <>
       {notes && (
@@ -64,11 +75,9 @@ const View = () => {
             <Col><h1>{notes.title}</h1></Col>
             <Col xs="auto">
               <Stack gap={2} direction="horizontal">
-                <Link to="/note/edit">
-                  <Button variant="primary">Edit</Button>
-                </Link>
-                <Button variant="outline-danger">Delete</Button>
-                <Button variant="outline-secondary" onClick={() => navigate('/note', {replace: true})}>Back</Button>
+                <Button variant="primary" onClick={() => navigate(`/note/edit${id}`, {replace: true})}><BsPencilSquare /></Button>
+                <Button variant="outline-danger" onClick={deleteNote}><BsFillTrashFill /></Button>
+                <Button variant="outline-secondary" onClick={() => navigate('/note', {replace: true})}><BiArrowBack /></Button>
               </Stack>
             </Col>
           </Row>
