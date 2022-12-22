281828610dbc215bdc38f6855fa116b282d11066
JackWorld <jackwong0326@gmail.com>
2022-12-22 23:36:30 +0800

    Search features: Assign task name list.

--- frontend/src/pages/Assign.js
+++ frontend/src/pages/Assign.js
@@ -1,9 +1,10 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ROLES } from '../config/roles'
 import { usePathContext } from '../context/path'
 import { useAuthContext } from '../context/auth'
 import { useTasksContext } from '../context/task'
 import { FaTasks } from 'react-icons/fa'
+import { GoSearch } from "react-icons/go"
 import { MdAdminPanelSettings } from "react-icons/md"
 import { useLocation } from 'react-router-dom'
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
@@ -11,6 +12,7 @@ import Details from '../components/tasks/assign/Index'
 import Add from '../components/tasks/assign/Add'
 
 const Assgin = () => {
+  const [ query, setQuery ] = useState("")
   const { auth } = useAuthContext()
   const { setTitle } = usePathContext()
   const { assignedUser, setAssignedUser } =  useTasksContext()
@@ -53,6 +55,11 @@ const Assgin = () => {
         <div className="bg-success bg-opacity-25 rounded pt-2">
           <span className="mx-3 d-inline-flex align-items-center"><FaTasks className="fs-4"/>&ensp;{title}</span>
           <span className="d-inline-flex align-items-center"><MdAdminPanelSettings className="fs-4"/>&ensp;{createdBy.name}</span>
+        </div>
+
+        <div className="input-group my-3">
+          <input type="search" className="form-control" placeholder="Search..." value={query} onChange={e => setQuery(e.target.value)}/>
+          <button className="btn btn-outline-primary" type="button"><GoSearch/></button>
         </div>
 
           {(assignedUser?.assignedTo?.length > 0) && (
