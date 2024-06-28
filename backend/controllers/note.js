6391626c4c4b1f6d12e908198d61c91b6388d795
JackWorld <jackwong0326@gmail.com>
2024-06-29 00:47:58 +0800

    Refactor(Note management): Custom error handling.

--- backend/controllers/note.js
+++ backend/controllers/note.js
@@ -1,62 +1,74 @@
-const mongoose = require('mongoose')
 const Note = require('../models/Note')
-const validator = require('validator')
 const ROLES_LIST = require('../config/rolesList')
+const { CustomError } = require('../middleware/errorHandler')
+const { validateAuthInputField, validateObjectId } = require('../utils/validation')
 
-exports.getAll = async (req, res) => {
-  const user_id = req.user._id
-
-  const notes = await Note.find({user_id}).sort({createdAt: -1}).lean()
-  if (!notes?.length) return res.status(400).json({ error: 'No notes record found' })
-
-  res.status(200).json(notes)
-}
-
-exports.adminGetAll = async (req, res) => {
-  const admin_id = req.user._id
-  const user_id = req.body.id
-
-  if(!user_id && (admin_id == user_id)) return res.status(400).json({ error: 'User id not found' })
-  if (!mongoose.Types.ObjectId.isValid(user_id)) return res.status(404).json({error: 'No such note id found'})
-
-  const notes = await Note.find({user_id: user_id}).sort({createdAt: -1}).lean()
-  if (!notes?.length) return res.status(400).json({ error: 'No notes record found' })
-
-  res.status(200).json(notes)
-}
-
-exports.getById = async (req, res) => {
-  const { id } = req.params
-  const user_id = req.user._id
-
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'No such note id found'})
-  
-  const note = await Note.find({'user_id': user_id, '_id': id}).lean().exec()
-  if (!note[0]) return res.status(404).json({error: 'No such note record found'})
-
-  res.status(200).json(note[0])
-}
-
-exports.adminGetById = async (req, res) => {
-  const note_id = req.body.id
-
-  if (!mongoose.Types.ObjectId.isValid(note_id)) return res.status(404).json({error: 'No such note id found'})
-  
-  const note = await Note.findById(note_id).lean().exec()
-  if (!note) return res.status(404).json({error: 'No such note record found'})
-
-  res.status(200).json(note)
-}
-
-exports.create = async (req, res) => {
-  const { title, text, tag } = req.body
-
+exports.getAll = async (req, res, next) => {
   try {
-    const isTitleEmpty = validator.isEmpty(title ?? "", { ignore_whitespace:true })
-    if (isTitleEmpty) throw Error('Title required')
+    const user_id = req.user._id
+  
+    const notes = await Note.find({ user_id }).sort({createdAt: -1}).lean()
+    if (!notes?.length) throw new CustomError('No notes record found', 404)
+  
+    res.status(200).json(notes)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.adminGetAll = async (req, res, next) => {
+  try {
+    const admin_id = req.user._id
+    const user_id = req.body.id
+  
+    if(!user_id && (admin_id == user_id)) throw new CustomError('User id not found', 404)
+    validateObjectId(user_id, 'Note')
+  
+    const notes = await Note.find({user_id: user_id}).sort({ createdAt: -1 }).lean()
+    if (!notes?.length) throw new CustomError('No notes record found', 404)
+  
+    res.status(200).json(notes)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getById = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user_id = req.user._id
+  
+    validateObjectId(id, 'Note')
     
-    const isTextEmpty = validator.isEmpty(text ?? "", { ignore_whitespace:true })
-    if (isTextEmpty) throw Error('Body required')
+    const note = await Note.find({ user_id: user_id, _id: id }).lean().exec()
+    if (!note[0]) throw new CustomError('No such note record found', 404)
+  
+    res.status(200).json(note[0])
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.adminGetById = async (req, res, next) => {
+  try {
+    const note_id = req.body.id
+  
+    validateObjectId(note_id, 'Note')
+    
+    const note = await Note.findById(note_id).lean().exec()
+    if (!note) throw new CustomError('No such note record found', 404)
+  
+    res.status(200).json(note)
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.create = async (req, res, next) => {
+  try {
+    const { title, text, tag } = req.body
+
+    validateAuthInputField({ title, text })
 
     const userId = req.user._id
     const targetUserId = req.body.id // user id that Admin use to update user record
@@ -67,50 +79,53 @@ exports.create = async (req, res) => {
     }
 
     const notes = await Note.create({ title, text, user_id: idToCreate, tag })
+    if(!notes) throw new CustomError('Something went wrong, during creating new note', 400)
+
     res.status(201).json(notes)
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    next(error)
   }
 }
 
-exports.update = async (req, res) => {
-  const { id } = req.params
-  const { title, text } = req.body
-
-  const isTitleEmpty = validator.isEmpty(title ?? "", { ignore_whitespace:true })
-  if (isTitleEmpty) return res.status(400).json({error: 'Title required'})
-
-  const isTextEmpty = validator.isEmpty(text ?? "", { ignore_whitespace:true })
-  if (isTextEmpty) return res.status(400).json({error: 'Body required'})
+exports.update = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { title, text } = req.body
   
-  const isIdEmpty = validator.isEmpty(id, { ignore_whitespace:true })
-  if (isIdEmpty) return res.status(400).json({error: 'Note id required'})
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({error: 'No such note id found'})
+    validateAuthInputField({ title, text })
+    validateObjectId(id, 'Note')
+  
+    const userId = req.user._id //normal record update id (user id/admin id) 
+    const targetUserId = req.body.id //user id that Admin use to update user record
+    let idToUpdate = userId
 
-  const userId = req.user._id //normal record update id (user id/admin id) 
-  const targetUserId = req.body.id //user id that Admin use to update user record
-  let idToUpdate = userId
-  if(targetUserId && (userId !== targetUserId) && (req.roles == ROLES_LIST.Admin)){
-    idToUpdate = targetUserId
+    if(targetUserId && (userId !== targetUserId) && (req.roles == ROLES_LIST.Admin)){
+      idToUpdate = targetUserId
+    }
+    
+    const notes = await Note.findOneAndUpdate({ _id: id }, { ...req.body }).lean().exec()
+    if (!notes) throw new CustomError('No such note record found', 404)
+    
+    //after update return new record
+    const updatedRecord = await Note.find({ user_id: idToUpdate }).sort({createdAt: -1}).lean()
+
+    res.status(200).json(updatedRecord)
+  } catch (error) {
+    next(error)
   }
-  
-  const notes = await Note.findOneAndUpdate({_id: id}, {...req.body }).lean().exec()
-  if (!notes) return res.status(400).json({error: 'No such note record found'})
-  
-  //after update return new record
-  const updatedRecord = await Note.find({user_id: idToUpdate}).sort({createdAt: -1}).lean()
-  res.status(200).json(updatedRecord)
 }
 
-exports.delete = async (req, res) => {
-  const { id } = req.params
-
-  const isIdEmpty = validator.isEmpty(id, { ignore_whitespace:true })
-  if (isIdEmpty) return res.status(400).json({error: 'Note id required'})
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({error: 'No such note id found'})
-
-  const note = await Note.findByIdAndDelete(id).lean().exec()
-  if(!note) return res.status(400).json({error: 'No such note record found'})
-
-  res.status(200).json(note)
+exports.delete = async (req, res, next) => {
+  try {
+    const { id } = req.params
+  
+    validateObjectId(id, 'Note')
+  
+    const note = await Note.findByIdAndDelete(id).lean().exec()
+    if(!note) throw new CustomError('No such note record found', 404)
+  
+    res.status(200).json(note)
+  } catch (error) {
+    next(error)
+  }
 }
